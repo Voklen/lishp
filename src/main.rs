0ad56b17ec6bfcd31f380ffaa03ef5ad6a6d53e8
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-09-15 10:05:16 +0100

    Add shell prompt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use std::io;
+use std::io::{self, Write};
 
 mod errors;
 mod executor;
 mod parsing;
 
 fn main() {
+	print_prompt();
 	let lines = io::stdin().lines();
 	for line in lines {
 		let parsed = parsing::parse(line);
 		executor::executor(parsed);
+		print_prompt();
+	}
+}
+
+fn print_prompt() {
+	print!("> ");
+	match io::stdout().flush() {
+		Ok(()) => {}
+		Err(e) => throw!("Error flushing stdout: {e}"),
 	}
 }
