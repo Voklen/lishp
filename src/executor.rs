ceaa078d2466d16d88ee6deb85ac6cea2e158631
Alex Gorichev <Alex.Gorichev@protonmail.com>
2023-09-16 20:53:34 +0100

    Implement parsing and execution

--- src/executor.rs
+++ src/executor.rs
@@ -1,12 +1,36 @@
 use std::process::Command;
 
-use crate::{parsing::Func, throw, unwrap_or_return};
+use crate::{
+	parsing::{Expression, Func},
+	throw, unwrap_or_return,
+};
 
 pub fn executor(option_func: Option<Func>) {
 	let func = unwrap_or_return!(option_func);
 
-	let startup_result = Command::new(func.name).args(func.arguments).spawn();
+	let args = get_args(func.arguments);
+	println!("{:?}", args);
+	let startup_result = Command::new(func.name).args(args).spawn();
 	let mut child = startup_result.unwrap_or_else(|e| throw!("Could not start command: {e}"));
 	let exit_code = child.wait();
 	exit_code.unwrap_or_else(|e| throw!("Exited with code: {e}"));
+}
+
+fn get_args(func: Vec<Expression>) -> Vec<String> {
+	func.into_iter().map(evaluate_expression).collect()
+}
+
+fn evaluate_expression(expr: Expression) -> String {
+	match expr {
+		Expression::String(str) => str,
+		Expression::Function(func) => evaluate_func(func),
+	}
+}
+
+fn evaluate_func(func: Func) -> String {
+	let args = get_args(func.arguments);
+	let startup_result = Command::new(func.name).args(args).output();
+	let child = startup_result.unwrap_or_else(|e| throw!("Could not start command: {e}"));
+
+	String::from_utf8_lossy(&child.stdout).trim().into()
 }
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,44 +1,72 @@
 use crate::throw;
-use std::{ffi::OsStr, io};
+use std::{io, iter::Peekable, str::Chars};
 
 pub fn parse(result_line: Result<String, io::Error>) -> Option<Func> {
 	let line = result_line.unwrap_or_else(|e| throw!("Error reading line: {e}"));
-	let mut tokens = line.split_whitespace();
-	let name = tokens.next()?.to_owned();
-	let arguments: Vec<Expression> = tokens
-		.map(|str| Expression::String(str.to_owned()))
-		.collect();
-	Some(Func { name, arguments })
+	let mut chars = line.chars().peekable();
+	let name = next_word(&mut chars);
+
+	let mut args = vec![];
+	loop {
+		let result = get_next_expression(&mut chars);
+		match result {
+			Some(expr) => args.push(expr),
+			None => break,
+		}
+	}
+	println!("name: {} args: {:?}", name, args);
+	Some(Func {
+		name: name.to_owned(),
+		arguments: args,
+	})
 }
 
+fn next_word(chars: &mut Peekable<Chars<'_>>) -> String {
+	let reference = chars.by_ref();
+	reference.take_while(|char| !char.is_whitespace()).collect()
+}
+
+/// Returns None if end of function
+fn get_next_expression(chars: &mut Peekable<Chars<'_>>) -> Option<Expression> {
+	let first_char = chars.peek()?;
+	match first_char {
+		'(' => Some(parse_function(chars)),
+		')' => {
+			chars.next();
+			None
+		}
+		_ => Some(Expression::String(next_word(chars))),
+	}
+}
+
+fn parse_function(chars: &mut Peekable<Chars<'_>>) -> Expression {
+	chars.next();
+	let name = next_word(chars);
+	let mut args = vec![];
+
+	loop {
+		let result = get_next_expression(chars);
+		match result {
+			Some(expr) => args.push(expr),
+			None => break,
+		}
+	}
+
+	let as_func = Func {
+		name: name.to_owned(),
+		arguments: args,
+	};
+	Expression::Function(as_func)
+}
+
+#[derive(Debug)]
 pub enum Expression {
 	String(String),
 	Function(Func),
 }
 
-impl Expression {
-	fn to_string(&self) -> &str {
-		match self {
-			Self::String(str) => str,
-			Self::Function(func) => func.to_string(),
-		}
-	}
-}
-
-impl AsRef<OsStr> for Expression {
-	fn as_ref(&self) -> &OsStr {
-		OsStr::new(self.to_string())
-	}
-}
-
+#[derive(Debug)]
 pub struct Func {
 	pub name: String,
 	pub arguments: Vec<Expression>,
-}
-
-impl Func {
-	fn to_string(&self) -> &str {
-		format!("{}", self.name);
-		"hello"
-	}
 }
